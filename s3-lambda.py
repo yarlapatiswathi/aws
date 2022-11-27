f9a5f57eacb85460d6348b538aafb0cb8c86551c
yarlapatiswathi <104160689+yarlapatiswathi@users.noreply.github.com>
2022-11-26 22:45:56 -0500

    Create s3-lambda.py
    Lambda gets triggered when a file is dropped in it and then it picks the object from s3 and prints its name and its contents

---
+++ s3-lambda.py
@@ -0,0 +1,18 @@
+# gets a s3 object and prints its contents
+import boto3
+def lambda_handler(event, context):
+    s3=boto3.client('s3')
+    
+    if event:
+        print('Event: ',event)
+        print('Event Type:',type(event))
+        file_obj=event['Records'][0]
+        filename=str(file_obj['s3']['object']['key'])
+        print('filename: ',filename)
+        s3obj=s3.get_object(Bucket='basic-lambda-s3',Key=filename)
+        print('Bucket object :',s3obj)
+        s3obj_content=s3obj['Body'].read().decode('utf-8')
+        print(s3obj_content)
+    return {
+        'statusCode': 200,
+        }
